c7506acdb532629b7b15070c806b1939ccdb0ab3
n00rs <007s.noorsha@gmail.com>
2022-12-28 20:47:34 +0530

    deploy 0.1.1

--- server/server.js
+++ server/server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const CurrencyConverter = require("./utils/currencyConverter");
-
 const app = express();
-
 const env = require("dotenv").config();
 const { PORT } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 
 
 app.use("/api/razorpay", require("./routes/razorpayRoutes"));
 app.use("/api/paypal", require("./routes/paypalRoutes"));
-app.use('/api/stripe',require('./routes/stripeRoutes'))
-app.use('/api/paytm',require('./routes/paytmRoutes'))
+app.use("/api/stripe", require("./routes/stripeRoutes"));
+app.use("/api/paytm", require("./routes/paytmRoutes"));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
